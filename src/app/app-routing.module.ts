8aab06ac549fff50cea54d609f14bcd749644264
Brandon McCray <bradmccray89@gmail.com>
2022-11-12 13:48:54 -0500

    added pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,21 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./pages/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'services',
+    loadChildren: () =>
+      import('./pages/services/services.module').then((m) => m.ServicesModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./pages/contact/contact.module').then((m) => m.ContactModule),
+  },
 ];
 
 @NgModule({
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  navLinks = [
+    { path: 'home', label: 'Home' },
+    { path: 'about', label: 'About' },
+    { path: 'services', label: 'Services' },
+    { path: 'contact', label: 'Contact' },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
